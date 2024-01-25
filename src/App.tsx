09bdfa682ea8756f173c4cd3df0b51cd928bf165
Victor Hernandez, Jr <vh4697@gmail.com>
2024-01-25 17:56:18 -0600

    created tic-tac-toe grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import './App.css'
+import Block from './components/Block'
+
 
 function App() {
 
-  return (
-    <></>
-  )
+    const [ input, setInput ] = useState(0);
+
+    const createGrid = () => {
+        let grid = [];
+        for (let i = 1; i <=9; i++) {
+            grid.push(<Block number={i} onClick={() => setInput(i)} />);
+        }
+        return grid;
+    }
+
+    return (
+        <div className='d-flex flex-column justify-content-center align-items-center '>
+            <div className="row row-cols-3">
+                { createGrid() }
+            </div>
+            <p className='text-center'>Input: {input}</p>
+        </div>
+    )
 }
 
 export default App
