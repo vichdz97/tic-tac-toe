550e0c4391b865d7c940261020edfd1e2b7121c9
Victor Hernandez, Jr <vh4697@gmail.com>
2024-01-27 02:42:03 -0600

    added reset button

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import Block from './components/Block'
 
 function App() {
 
+    const refresh = () => {
+        window.location.reload();
+    } 
+
     const createGrid = () => {
         let grid: Array<any> = [];
         for (let i = 1; i <= 9; i++) {
             grid.push(<Block key={i} />);
         }
         return grid;
-    }
+    }   
 
     return (
         <div className='d-flex flex-column justify-content-center align-items-center mx-1'>
             <h1>Tic-Tac-Toe</h1>
+            <button className='btn btn-light border border-dark mt-2 mb-4' onClick={refresh}>Reset</button>
             <div className="row row-cols-3 w-50">
                 { createGrid() }
             </div>
