397e410a1997026b9fec33c8149ee02b589028a5
Victor Hernandez, Jr <vh4697@gmail.com>
2024-01-27 02:39:54 -0600

    added images to Blocks

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import '../App.css'
 
-interface Props {
-    number: number;
-    onClick: () => void;
-}
+function Block() {
 
-function Block({ number, onClick }: Props) {
+    const CIRCLE = 'src/assets/circle.png';
+    const X = 'src/assets/x.png';
+    const [ image, setImage ] = useState('');
 
     return (
-        <div className={`block col border border-danger`} onClick={onClick} role="button">{number}</div>
+        <div className={`block col p-0 m-0 d-flex justify-content-center align-items-center`} 
+        onClick={() => setImage(CIRCLE)} 
+        role={image ? '' : 'button'}>
+            <img src={image} width='50' />
+        </div>
     )
 } 
 
