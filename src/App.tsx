e897f38737300a592f39b7fbd9d1c5c4c92f25c9
Victor Hernandez, Jr <vh4697@gmail.com>
2024-01-27 02:36:40 -0600

    refactored createGrid() & added heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { useState } from 'react';
 import './App.css'
 import Block from './components/Block'
 
-
 function App() {
 
-    const [ input, setInput ] = useState(0);
-
     const createGrid = () => {
-        let grid = [];
-        for (let i = 1; i <=9; i++) {
-            grid.push(<Block number={i} onClick={() => setInput(i)} />);
+        let grid: Array<any> = [];
+        for (let i = 1; i <= 9; i++) {
+            grid.push(<Block key={i} />);
         }
         return grid;
     }
 
     return (
-        <div className='d-flex flex-column justify-content-center align-items-center '>
-            <div className="row row-cols-3">
+        <div className='d-flex flex-column justify-content-center align-items-center mx-1'>
+            <h1>Tic-Tac-Toe</h1>
+            <div className="row row-cols-3 w-50">
                 { createGrid() }
             </div>
-            <p className='text-center'>Input: {input}</p>
         </div>
     )
 }
